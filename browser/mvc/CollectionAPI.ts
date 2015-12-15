053956c0ead26b494c8ff5064673773127919f31
ascheron <benjamin.andreosso@gmail.com>
2015-12-16 00:44:38 +0100

    WIP front messagerie

--- browser/mvc/CollectionAPI.ts
+++ browser/mvc/CollectionAPI.ts
@@ -22,19 +22,22 @@ namespace ghost.mvc
         }
         protected _getRequest(part?:string):APIExtended
         {
-            if(typeof part == "string")
+            if (typeof part !== "string")
             {
-                return this.requests[part];
-            }else
-            {
-                return  this.requests[ Model.PART_DEFAULT];
+                part = Model.PART_DEFAULT;
             }
-            return null;
+
+            if (!this.requests[part])
+            {
+                this.requests[part] = this.getRequest(part);
+            }
+
+            return this.requests[part];
         }
         public next(part:string, quantity:number):APIExtended
         public next(quantity:number):APIExtended
         public next():APIExtended
-        public next(part:any = null, quantity:number = 10):APIExtended
+        public next(part:any = null, quantity?:number):APIExtended
         {
             if(typeof part == "number")
             {
@@ -46,12 +49,13 @@ namespace ghost.mvc
             {
                 return request.next(quantity);
             }
+
             return null;
         }
         public nextAll(part:string, quantity:number):APIExtended
         public nextAll(quantity:number):APIExtended
         public nextAll():APIExtended
-        public nextAll(part:any = null, quantity:number = 10):APIExtended
+        public nextAll(part:any = null, quantity?:number):APIExtended
         {
             if(typeof part == "number")
             {
@@ -68,7 +72,7 @@ namespace ghost.mvc
         public previous(part:string, quantity:number):APIExtended
         public previous(quantity:number):APIExtended
         public previous():APIExtended
-        public previous(part:any = null, quantity:number = 10):APIExtended
+        public previous(part:any = null, quantity?:number):APIExtended
         {
             if(typeof part == "number")
             {
@@ -85,7 +89,7 @@ namespace ghost.mvc
         public previousAll(part:string, quantity:number):APIExtended
         public previousAll(quantity:number):APIExtended
         public previousAll():APIExtended
-        public previousAll(part:any = null, quantity:number = 10):APIExtended
+        public previousAll(part:any = null, quantity?:number):APIExtended
         {
             if(typeof part == "number")
             {
@@ -103,6 +107,48 @@ namespace ghost.mvc
         {
             return this._partsPromises[name] || this.getRequest(name, params)!=null;//name == "default";
         }
+
+        public retrieveData(data:string[] = [Collection.PART_DEFAULT], params:any = null):Promise<any>
+        {
+            if(!data)
+            {
+                data = [Collection.PART_DEFAULT];
+            }
+            var _this:Collection<any> = this;
+            var promise:Promise<any> = new Promise<any>(function(accept:any, reject:any):void
+            {
+
+                var failed:boolean = false;
+                var promises:Promise<any>[] = data.map(function(name:string)
+                {
+                    if(this.hasPart(name, params))
+                    {
+                        return this.getPartPromise(name, params);
+                    }else
+                    {
+                        //reject Promise
+                        failed = true;
+                        reject(new Error(name+" is not a correct part's name"));
+                        return null;
+                    }
+                }, _this);
+                if(failed)
+                {
+                    return;
+                }
+               Promise.all(promises).then(function(values:any[])
+                {
+                    debugger;
+                    //TODO:weird le data.read devrait être dans le filter ?
+                    values.filter(function(data:any):boolean{ return data!==true && !data.read?true:false;}).map(function(data:any){
+                        data.read = true;
+                        return data.data[0];})//.forEach(this.readExternal, _this);
+                    accept();
+                }.bind(_this), reject);
+            });
+            return promise;
+        }
+
         protected getPartPromise(name:string, params:any = null):Promise<any>|boolean
         {
             if(!this.hasPart(name, params))
@@ -112,6 +158,9 @@ namespace ghost.mvc
             if(!this._partsPromises[name])
             {
                 var request:API<API<any>> = this.getRequest(name, params);
+
+                request.on(API.EVENT_DATA_FORMATTED, this.onPartData.bind(this, name));
+
                 this.requests[name] = request;
 
                 this._partsPromises[name] = new Promise<any>((accept, reject)=>
@@ -123,7 +172,6 @@ namespace ghost.mvc
                             _self._partsPromises[name] = true;
                             accept.call(null, {data:Array.prototype.slice.call(arguments),read:false});
 
-                            request.on(API.EVENT_DATA_FORMATTED, _self.onPartData.bind(_self, name));
                         },reject);
                 });
             }
@@ -151,7 +199,8 @@ namespace ghost.mvc
 
         public readExternal(input:any[]):void
         {
-            debugger;
+            super.readExternal(input);
+            // debugger;
         }
     }
 }
