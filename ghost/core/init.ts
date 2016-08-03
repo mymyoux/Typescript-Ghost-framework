eb59442f8f45834caa2a0136129037190c913a24
mymyoux <mymyoux@gmail.com>
2016-08-03 18:56:04 +0200

    new apply mixins implementation

--- ghost/core/init.ts
+++ ghost/core/init.ts
@@ -65,9 +65,33 @@ namespace ghost
     
 }
 function applyMixins(derivedCtor: any, baseCtors: any[]) {
-    baseCtors.forEach(baseCtor => {
-        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
-            derivedCtor.prototype[name] = baseCtor.prototype[name];
-        })
-    });
+    if(!derivedCtor || !baseCtors)
+    {
+        return;
+    }
+    if (derivedCtor.prototype && typeof derivedCtor == "function")
+    {
+        baseCtors.forEach(baseCtor => {
+            if (baseCtor.prototype.applyMixin) {
+                baseCtor.prototype.applyMixin(derivedCtor.prototype);
+            } else {
+                Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+                    derivedCtor.prototype[name] = baseCtor.prototype[name];
+                });
+            }
+        });
+    }else
+    {
+        baseCtors.forEach(baseCtor => {
+            if (baseCtor.prototype.applyMixin)
+            {
+                baseCtor.prototype.applyMixin(derivedCtor);
+            }else
+            {
+                Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+                    derivedCtor[name] = baseCtor.prototype[name];
+                });
+            }
+        });
+    }
 }
