00effa7017da88a353d1780138af0413ee46af70
mymyoux <mymyoux@gmail.com>
2017-02-23 18:34:13 +0100

    beanstalkd

---
+++ ghost/queue/Beanstalk.ts
@@ -0,0 +1,140 @@
+///<lib="node"/>
+///<module="events"/>
+///<module="database"/>
+namespace ghost.queue {
+    var beanstalk = require("fivebeans");
+    import Database = ghost.database.Database;
+    export class Beanstalk extends ghost.events.EventDispatcher
+    {
+        public static EVENT_CONNECTED:string = "connected";
+        public static EVENT_ERROR:string = "error";
+        public static EVENT_DISCONNECTED:string = "disconnected";
+        private static _instance: Beanstalk;
+
+
+        public static instance():Beanstalk
+        {
+            if (!Beanstalk._instance)
+            {
+                Beanstalk._instance = new Beanstalk();
+            }
+            return Beanstalk._instance;
+        }
+        protected beanstalk: any;
+        protected connected:boolean;
+        protected watched: any = {};
+        public constructor()
+        {
+            super();
+            this.connected = false;
+        }
+        public config(options:any):void
+        {
+            this.beanstalk = new beanstalk.client(options.ip, options.port);
+            this.beanstalk.on('connect', this.onConnect.bind(this));
+            this.beanstalk.on('error', this.onError.bind(this));
+            this.beanstalk.on('close', this.onClose.bind(this));
+            this.beanstalk.connect();
+        }
+        public onConnect()
+        {
+            this.connected = true;
+            console.log("beantalkd - connected");
+            this.trigger(Beanstalk.EVENT_CONNECTED);
+        }
+        public onError(error)
+        {
+            console.error("beanstalkd error - ",error);
+            this.trigger(Beanstalk.EVENT_ERROR,error);
+        }
+        public onClose()
+        {
+            this.connected = false;
+            console.log("beantalkd - closed"); 
+            this.trigger(Beanstalk.EVENT_DISCONNECTED);
+        }
+        public next(tube:string, callback:any):void
+        {
+            if(!this.watched[tube])
+            {
+                this.watch(tube, ()=>
+                {
+                    this.next(tube, callback);
+                });
+                return;
+            }
+            this.reserve(callback);
+        }
+        public watch(tube:string, callback:any):void
+        {
+            if(!this.connected)
+            {
+                this.once(Beanstalk.EVENT_CONNECTED, ()=> {
+                    this.watch(tube, callback);
+                });
+                return;
+            }
+            this.watched[tube] = true;
+            this.beanstalk.watch(tube, callback);
+        }
+        public reserve(callback:any):void
+        {
+            this.beanstalk.reserve( (error:any, id:string, payload:any):void=>
+                {
+                    if(error)
+                    {
+                        console.error("beanstalk reserve error:", error);
+                        return;
+                    }
+                    var data:any = JSON.parse(payload.toString('utf8'));
+                    if(data && data._id_beanstalkd)
+                    {
+                        Database.instance().query('SELECT * FROM beanstalkd_log WHERE id=?', [data._id_beanstalkd], (error, results, fields)=>
+                        {
+                             if(error)
+                             {
+                                 console.error("beanstalk reserve mysql error:", error);
+                                 return;
+                             }   
+                             if (!results.length)
+                             {
+                                 console.error("beanstalk mysql error: no beanstalkd with id = ", data._id_beanstalkd);
+                                 return;
+                             }
+                             var job: Job = new Job(this, id, data, results[0]);
+                             Database.instance().query('UPDATE beanstalkd_log SET state=?,tries=? WHERE id=?', [job.state, job.tries, job.id_database], function(error) 
+                             {
+                                 if (error) {
+                                     console.error("beanstalk reserve mysql update error:", error);
+                                     return;
+                                 }   
+                                 callback(job);
+                             });
+                        });
+                    }else
+                        callback(new Job(this, id, data));
+                });
+        }
+        public success(job: Job): void
+        {
+            this.beanstalk.destroy(job.id, function() { });
+             this.updateJob(job);
+        }
+        public fail(job:Job):void
+        {
+            if(job.tries<3)
+            {
+                this.beanstalk.release(job.id, 0, 60,function(){});
+            }else{
+                this.beanstalk.bury(job.id, 0, function() { });
+            }
+           this.updateJob(job);
+        }
+        protected updateJob(job:Job):void
+        {
+            if (job.id_database) {
+                Database.instance().query('UPDATE beanstalkd_log SET state=?, duration=? WHERE id=?', [job.state, job.getDuration(),job.id_database]);
+            }
+        }
+    }
+}
---
+++ ghost/queue/Job.ts
@@ -0,0 +1,53 @@
+///<lib="node"/>
+namespace ghost.queue {
+    export class Job{
+      protected beanstalk: Beanstalk;
+      public static STATE_EXECUTING: string = "executing";
+      public static STATE_EXECUTED: string = "executed";
+      public static STATE_FAILED: string = "failed";
+      public static STATE_RETRYING: string = "retrying";
+
+      public id: string;
+      public data: any;
+      public tries: number = 0;
+      public state: string;
+      public id_database: string;
+      public start_time: number;
+      public constructor(beanstalk:Beanstalk, id:string, data:any, databaseData?:any)
+      {
+          this.beanstalk = beanstalk;
+          this.id = id;
+          if (databaseData)
+          {
+            this.data = JSON.parse(databaseData["json"]);
+            this.tries = parseInt(databaseData["tries"], 10);
+            this.state = databaseData["state"];
+            this.id_database = databaseData["id"];
+          }else{
+            this.data = data;
+          }
+          this.tries++;
+          this.state = Job.STATE_EXECUTING;
+          this.start_time = Date.now();
+      }
+      public success()
+      {
+          this.state = Job.STATE_EXECUTED;
+          this.beanstalk.success(this);
+      }
+      public fail()
+      {
+        if(this.tries<3)
+        {
+          this.state = Job.STATE_RETRYING;
+        }else{
+          this.state = Job.STATE_FAILED;  
+        }
+        this.beanstalk.fail(this);
+      }
+      public getDuration():number
+      {
+        return Date.now() - this.start_time;
+      }
+    }
+}
