93ea3e0b64104c6fe4b9d444a57e48a0bce63985
Wendy <wendy.gureghian@epitech.eu>
2017-07-18 11:37:14 +0200

    component.ts

--- browser/mvc2/Component.ts
+++ browser/mvc2/Component.ts
@@ -418,7 +418,8 @@ export class Component extends CoreObject
             this.vueConfig.data[name] = value;
             return;
         }
-        this.template.$set(this.template, name, value);
+        if (this.template)
+            this.template.$set(this.template, name, value);
     }
     protected $getData(name:string):any
     {
