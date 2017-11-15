b651f9e813db14af99eb4809c5d03281bd6c15a4
mymyoux <mymyoux@gmail.com>
2017-11-15 12:12:20 +0100

    prevent double load

--- browser/mvc2/Router.ts
+++ browser/mvc2/Router.ts
@@ -526,6 +526,7 @@
 			{
 				console.log("[router:href]" + jqueryEvent.currentTarget.getAttribute("href") + ' ' + target);
 			
+				jqueryEvent.preventDefault();
 				window.open(jqueryEvent.currentTarget.getAttribute("href"), target);
 				return;
 			}
