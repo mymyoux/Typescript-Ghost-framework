9ed0785b4548cc784bcafcde1d0ffadb3e0491a0
mymyoux <mymyoux@gmail.com>
2017-12-01 20:16:50 +0100

    clean

--- browser/helpers/LinkHelper.ts
+++ browser/helpers/LinkHelper.ts
@@ -24,8 +24,6 @@
             });
              $(document).on("click", ".link-inside", function(event, originalEvent = null)
             { 
-                console.log("on href:link-inside");
-                debugger;
               if(originalEvent)
               {
                   //already triggered
