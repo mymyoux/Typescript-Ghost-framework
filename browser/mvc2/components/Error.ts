708fcf65833a64223959751cbb5c31ea84be4a7f
ascheron <benjamin.andreosso@gmail.com>
2018-02-05 11:05:14 +0100

    error management in laravel

--- browser/mvc2/components/Error.ts
+++ browser/mvc2/components/Error.ts
@@ -17,20 +17,45 @@ export class ErrorComponent extends Component
         return {
         };
     }
-     protected bindVue():void
+    
+    protected bindVue():void
     {   
+        this.$addData('is_api', true);
+        this.$addData('front', true);
         this.$addModel(ErrorCollection);
         this.loadOneHour();
     }
+    
+    public $setIsApi( value : boolean = null ) : void
+    {
+        // null = both
+        // false = only non api errors
+        // true = only api errors
+        this.$addData('is_api', value);
+
+        this.$onErrorUnselected();
+    }
+
+    public $setFront( value : boolean ) : void
+    {
+        this.$addData('front', value);
+
+        this.$onErrorUnselected();
+    }
+
     public $onErrorSelected(item:ErrorModel):void
     {
         this.template.errors.clear();
         this.template.errors.order(["count","last_created_time"],[-1, -1]);
-        this.template.errors.loadGet({start:item.timestamp, end:item.timestamp+60*5});
+        this.template.errors.loadGet({
+            start   : item.timestamp,
+            end     : item.timestamp + 60 * 5,
+            is_api  : this.$getData('is_api'),
+            front   : this.$getData('front'),
+        });
     }
     public $onErrorUnselected():void
     {
-       
         this.template.errors.clear();
         this.template.errors.order(['last_created_time'], [-1]);
         this.loadOneHour();
@@ -42,8 +67,13 @@ export class ErrorComponent extends Component
         time.setMilliseconds(0);
         time.setMinutes(time.getMinutes()-time.getMinutes()%5);
         time.setMinutes(time.getMinutes()+5);
-        this.$getModel("errors").loadGet({start:time.getTime()-60*1000*5*12,
-            end:time.getTime(),step:60*5});
+        this.$getModel("errors").loadGet({
+            start   : time.getTime() - 60 * 1000 * 5 * 12,
+            end     : time.getTime(),
+            step    : 60 * 5,
+            is_api  : this.$getData('is_api'),
+            front   : this.$getData('front'),
+        });
     }
     public bindEvents():void
     {
